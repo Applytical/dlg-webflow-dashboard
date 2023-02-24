eefeea05d6898145bad46c0ac59c7a6330733d31
SImon Thow <104422191+SimonThow@users.noreply.github.com>
2023-02-24 16:27:52 +0000

    Update webflow-edit-subscriptions.js

--- webflow-edit-subscriptions.js
+++ webflow-edit-subscriptions.js
@@ -146,11 +146,10 @@ subscriptionCancel.addEventListener("click", function (e) {
         var ele = document.getElementsByName('reasons');
 
         for (i = 0; i < ele.length; i++) {
-          if (ele[i].checked)
-            document.getElementById("result").innerHTML
-              = "Gender: " + ele[i].value;
-        }
-        return ele[i].value;
+          if (ele[i].checked){
+            return ele[i].value;
+
+        }}
       }
       const reason = displayRadioValue();
       console.log(reason);
