30a2c6c8143455b84c38d28128951d7993c23db6
James Cartwright <james@weareandbeyond.com>
2023-03-08 23:05:20 +0000

    Dashboard Tour

---
+++ dashboard-tour.js
@@ -0,0 +1,7 @@
+introJs()
+  .addStep({
+    title: "Welcome to your subscription management dashboard!",
+    intro: "You now have one easy place to manage all your Livingood Daily subscriptions."
+  });
+introJs()
+  .setOption("dontShowAgain", true).start();
