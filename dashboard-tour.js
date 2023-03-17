eb236837d05f9574a6a32f90dae671e4818af2b5
James Cartwright <james@weareandbeyond.com>
2023-03-17 16:23:58 +0000

    Updateed tour trigger

--- dashboard-tour.js
+++ dashboard-tour.js
@@ -1,7 +1,35 @@
-introJs()
-  .addStep({
-    title: "Welcome to your subscription management dashboard!",
-    intro: "You now have one easy place to manage all your Livingood Daily subscriptions."
-  });
-introJs()
-  .setOption("dontShowAgain", true).start();
+function init() {
+  // Hide tour if already shown
+  if (getCookie('dlg.hidetour') == "true") return false;
+
+  // Start tour
+  introJs().start();
+
+  // SEt cookie to stop tour triggering again
+  setCookie('dlg.hidetour', true, 30);
+}
+
+init();
+
+// Cookie helper functions
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
