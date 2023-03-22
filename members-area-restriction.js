1ab2be97d015ee52a466c6d0cb7a85f25f1d8d8a
SImon Thow <104422191+SimonThow@users.noreply.github.com>
2023-03-22 11:48:56 +0000

    Update members-area-restriction.js

--- members-area-restriction.js
+++ members-area-restriction.js
@@ -1,10 +1,12 @@
-const shopifyTags = sessionStorage.getItem("shopifyTags");
-if (shopifyTags) {
-   var search = "Livingood Daily Lifestyle";
-   var arr = shopifyTags.split(", ");
-   if (arr.indexOf(search) == -1) {
-      window.location.href = "/no-membership";
-   }
-} else {
-   window.location.href = "/no-membership";
+if (window.location.pathname.includes('/members/')) {
+    const shopifyTags = sessionStorage.getItem("shopifyTags");
+    if (shopifyTags) {
+        var search = "Livingood Daily Lifestyle";
+        var arr = shopifyTags.split(", ");
+        if (arr.indexOf(search) == -1) {
+            window.location.href = "/no-membership";
+        }
+    } else {
+        window.location.href = "/no-membership";
+    }
 }
