c4d45e8433278dfdeb3dd9f005fe4dce68b2d446
SImon Thow <104422191+SimonThow@users.noreply.github.com>
2023-03-22 15:38:03 +0000

    Update members-area-restriction.js

--- members-area-restriction.js
+++ members-area-restriction.js
@@ -1,20 +1,23 @@
 const membersUrl = [
     "/training/",
-"/grocery-store-tour/",
-"/grocery-store-tour-videos/",
-"/daily-challenge/",
-"/daily-challenge-resources/",
-"/health/",
-"/advanced-challenges/",
-"/workout-videos/",
-"/workouts/",
-"/meal-plans/",
-"/masterclassses/",
-"/masterclass-videos/",
-"/product-link/",
-"/resources/",
-"/members/",
-"/members-welcome"
+    "/grocery-store-tour/",
+    "/grocery-store-tour-videos/",
+    "/daily-challenge/",
+    "/daily-challenge-resources/",
+    "/health/",
+    "/advanced-challenges/",
+    "/workout-videos/",
+    "/workouts/",
+    "/meal-plans/",
+    "/masterclassses/",
+    "/masterclass-videos/",
+    "/product-link/",
+    "/resources/",
+    "/members/",
+    "/members-welcome",
+    "/membership/",
+    "/membership"
+
 ];
 
 console.log(window.location.pathname);
@@ -31,6 +34,26 @@ if (membersUrl.some(url => window.location.pathname.includes(url))) {
         window.location.href = "/no-membership";
     }
 
+    const name = sessionStorage.getItem("name");
+    const avatar = showAvatar(name);
+
+
+    const navbar = document.getElementById('members-navigation').style.display = "block";
+    const membersAreaLink = document.getElementById('members-area-link').style.display = "block";
+
+
+} else if (membersUrl.some(url => window.location.pathname.includes("no-membership"))) {
+    const name = sessionStorage.getItem("name");
+    const avatar = showAvatar(name);
+}
+function membersIntitals(fullName) {
+    const namesArray = fullName.trim().split(' ');
+    if (namesArray.length === 1) return `${namesArray[0].charAt(0)}`;
+    else return `${namesArray[0].charAt(0)}${namesArray[namesArray.length - 1].charAt(0)}`;
+}
+
+function showAvatar(name) {
+
     let membersAreaIntitals
 
     const name = sessionStorage.getItem("name");
@@ -46,13 +69,4 @@ if (membersUrl.some(url => window.location.pathname.includes(url))) {
     }
 
     const avatar = document.getElementById('avatarImage').textContent = membersAreaIntitals;
-
-
-    const navbar = document.getElementById('members-navigation').style.display = "block";
-
-}
-function membersIntitals(fullName) {
-    const namesArray = fullName.trim().split(' ');
-    if (namesArray.length === 1) return `${namesArray[0].charAt(0)}`;
-    else return `${namesArray[0].charAt(0)}${namesArray[namesArray.length - 1].charAt(0)}`;
 }
