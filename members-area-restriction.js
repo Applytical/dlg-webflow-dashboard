0a08063dfda3e4959a15e9d03171a05a0b31f51f
SImon Thow <104422191+SimonThow@users.noreply.github.com>
2023-03-22 15:08:48 +0000

    Update members-area-restriction.js

--- members-area-restriction.js
+++ members-area-restriction.js
@@ -1,4 +1,22 @@
-if (window.location.pathname.includes('/members/')) {
+const membersUrl = [
+    "/training/",
+"/grocery-store-tour/",
+"/grocery-store-tour-videos/",
+"/daily-challenge/",
+"/daily-challenge-resources/",
+"/health/",
+"/advanced-challenges/",
+"/workout-videos/",
+"/workouts/",
+"/meal-plans/",
+"/masterclassses/",
+"/masterclass-videos/",
+"/product-link/",
+"/resources/",
+"/members/",
+]
+
+if (window.location.pathname.includes(membersUrl)) {
     const shopifyTags = sessionStorage.getItem("shopifyTags");
     if (shopifyTags) {
         var search = "Livingood Daily Lifestyle";
@@ -9,4 +27,29 @@ if (window.location.pathname.includes('/members/')) {
     } else {
         window.location.href = "/no-membership";
     }
+
+    function membersIntitals(fullName) {
+        const namesArray = fullName.trim().split(' ');
+        if (namesArray.length === 1) return `${namesArray[0].charAt(0)}`;
+        else return `${namesArray[0].charAt(0)}${namesArray[namesArray.length - 1].charAt(0)}`;
+    }
+    let membersAreaIntitals
+
+    const name = sessionStorage.getItem("name");
+
+    if (name) {
+        membersAreaIntitals = membersIntitals(name);
+        const UserName = document.getElementById('UserName').textContent = name;
+    } else {
+        const email = sessionStorage.getItem("email");
+        membersAreaIntitals = membersIntitals(email);
+        const EmailField = document.getElementById('email').textContent = email;
+
+    }
+
+    const avatar = document.getElementById('avatarImage').textContent = membersAreaIntitals;
+
+
+    const navbar = document.getElementById('members-navigation').style.display = "block";
+
 }
